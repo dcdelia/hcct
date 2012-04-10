60b21b9d71f867afdb42c6691d4cb93b5cfeab9c
danielecono.delia@gmail.com <danielecono.delia@gmail.com@ee1eabb9-e363-2533-fa49-3be757a96186>
2012-04-10 19:01:14 +0000

    cct with static bursting

--- testing/common.h
+++ testing/common.h
@@ -11,6 +11,8 @@
 #define DUMP_TREE		1
 
 #if BURSTING==1
+#define SAMPLING_INTERVAL   10*1000000
+#define BURST_LENGTH        1*1000000
 typedef struct hcct_stack_node_s hcct_stack_node_t;
 struct hcct_stack_node_s {
     UINT32           routine_id;    
